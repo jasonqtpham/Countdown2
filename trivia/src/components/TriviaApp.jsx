f5d1d0eda298bcf5c8696ee1c8f22d62f5b6308b
Jason Pham <deh5yb@virginia.edu>
2024-04-19 15:04:51 -0400

    fixed refresh button so choices also update

--- trivia/src/components/TriviaApp.jsx
+++ trivia/src/components/TriviaApp.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import '../styles/TriviaApp.css';
 const TriviaApp = () => {
   const [questions, setQuestions] = useState([]);
+  const [refresh, setRefresh] = useState(0);
   useEffect(() => {
     const fetchData = async () => {
         const response = await fetch('https://the-trivia-api.com/v2/questions');
@@ -9,14 +10,16 @@ const TriviaApp = () => {
         setQuestions(json);
     }
     fetchData();
-  }, []); 
+    console.log(refresh)
+  }, [refresh]); 
 
   return (
     <>
+    <button onClick={() => setRefresh(refresh+1)}>Refresh Questions</button>
     <div>
         {questions.map((question, index) => (
         <div key={index}>
-          <RenderQuestion questionInput={question} index={index}/>
+          <RenderQuestion question={question} index={index}/>
         </div>
       ))}
     </div>
@@ -24,23 +27,24 @@ const TriviaApp = () => {
   );
 }
 
-const RenderQuestion = ({questionInput, index}) => {
-    const [question,setQuest] = useState(questionInput);
-    const [shuffledChoices, setShuffledChoices] = useState(() => {
-      const combinedChoices = [question.correctAnswer, ...question.incorrectAnswers]
-      return combinedChoices.sort(() => 0.5 - Math.random());
-    });
+const RenderQuestion = ({question, index}) => {
+    const [shuffledChoices, setShuffledChoices] = useState([]);
+  
+    useEffect(() => {
+        const combinedChoices = [question.correctAnswer, ...question.incorrectAnswers];
+        setShuffledChoices(combinedChoices.sort(() => 0.5 - Math.random()));
+    }, [question]);
 
     const [selectedAnswer, setSelectedAnswer] = useState(null);
-    const handleChoiceSelection = (choice) => {
-      setSelectedAnswer(choice.target.value);
+    const handleChoiceSelection = (event) => {
+      setSelectedAnswer(event.target.value);
     };
     
     const choices = shuffledChoices.map((choice, index) => {
         const isCorrect = (choice === question.correctAnswer);
         const isSelected = (choice === selectedAnswer);
         const choiceClassName = isSelected ? (isCorrect ? 'correct' : 'incorrect') : '';
-        
+
         return (
           <div key={index} className={choiceClassName}>
               <input
@@ -59,8 +63,7 @@ const RenderQuestion = ({questionInput, index}) => {
         <>
         <div>
         <p>Question {index + 1}: {question.question.text}</p>
-            {choices}
-            
+            {choices} 
         </div>
         </>
     )
